6662474da710db44cc237ca1aa27120aa161c99e
Jonathan B <jburk@gala.games>
2024-11-21 15:05:17 -0600

    Adding fees

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,10 @@
+import type { TokenClassKeyProperties } from '@gala-chain/api'
+
 export const MAX_WINNERS = 10000
+
+export const GALA: TokenClassKeyProperties = {
+  additionalKey: 'none',
+  category: 'Unit',
+  collection: 'GALA',
+  type: 'none'
+}
